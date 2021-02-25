0bbb9ef6a1c946d39aadd6c97c9a94b2f9719e82
rizjiy <kirmenyov@mail.ru>
2021-02-26 02:12:11 +0300

    added: button for lanebt navbar menu media for 320px and more

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-var navbarButton = document.querySelector(".navbar-button");
-navbarButton.addEventListener('click', function() {
-    console.log("Клик");
-    document.querySelector(".header__login")
-    .classList.toggle("header__login--visible");
-});
+var main = function() { 
+
+    $('.navbar-button').click(function() {
+        $('.header__login').animate({
+            right: '0px'
+        }, 200); 
+
+    });
+
+    $('.navbar-button__close').click(function() { 
+        $('.header__login').animate({ 
+            right: '-285px'
+        }, 200); 
+    });
+};
+
+$(document).ready(main);
